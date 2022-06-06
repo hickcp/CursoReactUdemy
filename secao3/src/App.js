1b672455c2f8596760f973201366baed58266e70
hickcp <henrique_c_p@outlook.com>
2022-06-05 21:51:54 -0300

    "updated activies"

---
+++ secao3/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Seção 3</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="cidade.jpg" alt="Cidade" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
